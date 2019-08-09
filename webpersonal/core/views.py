3283fdf6cf3620ae5002d3c99aa5c1eba91cb857
sergio espinosa <sergio.es0709@gmail.com>
2019-08-09 15:10:06 -0300

    Añadido Proyecto

---
+++ webpersonal/core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,HttpResponse
+html_base="""
+<h1>Mi Web Personal</h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about">Acerca de</a></li>
+    <li><a href="/portfolio">Portafolio</a></li>
+    <li><a href="/contact">Contacto</a></li>   
+"""
+def home(request):
+    return render(request,"core/home.html",)
+def abaout(request):
+    return render(request,'core/about.html')
+def contact(request):
+    return render(request,'core/contact.html')
+# Create your views here.
